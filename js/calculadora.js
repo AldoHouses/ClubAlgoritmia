6122fd3e946e3557723e046731d6675af424262e
AldoCA27 <72528576+AldoCA27@users.noreply.github.com>
2021-04-14 16:30:49 -0500

    Add files via upload

---
+++ js/calculadora.js
@@ -0,0 +1,22 @@
+//Cualquier funcion que se ponga fuera de las funciones se ejecutan
+function holaMundo(){
+    alert("Hola Mundo del JS 1");
+    alert("Hola Mundo del JS 2");
+}
+
+function sumaNumeros(){
+    alert("Esta funcion suma los 2 numeros");
+    alert (document.getElementById("numero1"));
+}
+
+function restaNumeros(){
+    alert("Esta funcion resta los 2 numeros");
+}
+
+function multiplicaNumeros(){
+    alert("Esta funcion multiplica los 2 numeros");
+}
+
+function divisionNumeros(){
+    alert("Esta funcion divide los 2 numeros");
+}
